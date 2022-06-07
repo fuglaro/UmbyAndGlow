4d6ab0e9972b2b949d48220d3fe084a5b84b4bf6
John van Leeuwen <jvl@convex.cc>
2022-06-07 20:30:40 +0100

    pre-git history

--- Games/Umby&Glow/Umby&Glow.py
+++ Games/Umby&Glow/Umby&Glow.py
@@ -9,27 +9,14 @@ VIEW_H = 5 # height is in bytes (8 pixels)
 # Draw a layer with the same format as the view for the next frame.
 # Blocks to next frame.
 # Consider passing a memoryview for efficiency.
-# @param key: -1 -> [1: on, 0: off], 1 -> [1: on, 0: ignored], 0 -> [1: ignored, 0: off] 
-def display(layer, key):
+def display(layer):
     for x in range(0, VIEW_W):
-        thumby.display.blit(memoryview(layer)[x*5:(x+1)*5], x, 0, 1, 40, -1, 0, 0)
+        thumby.display.blit(memoryview(layer)[x*VIEW_H:(x+1)*VIEW_H], x, 0, 1, VIEW_H*8, -1, 0, 0)
     thumby.display.update()
 
 
 ##
-# Print a layer.
-# Similar to "display" but sends to stdout and doesn't block.
-# Intended for debugging.
-def print_display(layer): 
-    for y in range(0, VIEW_H):
-        for b in range(0, 8):
-            for x in range(0, VIEW_W):
-                print("#" if layer[x*VIEW_H+y] & (1<<b) else " ", end="")
-            print("")
-
-
 # Layers - 1 bit per pixel from top left, descending then wrapping to the right
-view = bytearray(VIEW_W*VIEW_H) # final composited display layer
 back = bytearray(VIEW_W*VIEW_H) # non-interaction background layer
 cave = bytearray(VIEW_W*VIEW_H) # non-interaction middle ground layer
 cavefill = bytearray(VIEW_W*VIEW_H) # non-interaction middle ground layer (cleared pixels)
@@ -41,14 +28,40 @@ glow = bytearray(VIEW_W*VIEW_H) # the player: glow
 
 
 
-view[5] = 5
-view[35] = 5
-view[107] = 5
-view[159] = 5
-view[35*5+4] = 3
 
-print_display(memoryview(view))
-display(memoryview(view), -1)
+##
+# Drawable pattern - dotted vertical lines repeating
+def walls_pattern(x, y):
+    return 0 if (x % 16) or (y % 2) else 1
+
+##
+# Drawable pattern - basic flat roof and floor
+def room_pattern(x, y):
+    return 1 if (y < 1) or (y >= VIEW_H*8 - 1) else 0
+
+def fill(pattern, layer, offsetX, offsetY):
+    for x in range(0, VIEW_W):
+        for y in range(0, VIEW_H):
+            for b in range(0, 8):
+                layer[x*VIEW_H+y] |= pattern(x + offsetX, (y + offsetY)*8+b) << b
+
+
+
+
+cave[5] = 5
+cave[35] = 5
+cave[107] = 5
+cave[159] = 5
+cave[35*5+4] = 3
+
+fill(walls_pattern, memoryview(back), 0, 0)
+fill(room_pattern, memoryview(land), 0, 0)
+
+
+# Composite a view with new frame data, drawing to screen
+display(memoryview(bytearray(
+    back[b] | land[b]
+    for b in range(0, VIEW_W*VIEW_H))))
 
 
 
