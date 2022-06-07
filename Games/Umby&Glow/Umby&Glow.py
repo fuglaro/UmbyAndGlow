3cfa4b7c00fc3468e6b193c1c8f20446722f5150
John van Leeuwen <jvl@convex.cc>
2022-06-07 20:32:14 +0100

    pre-git history

--- Games/Umby&Glow/Umby&Glow.py
+++ Games/Umby&Glow/Umby&Glow.py
@@ -6,14 +6,6 @@ VIEW_H = 5 # height is in bytes (8 pixels)
 MID = int(VIEW_H * 8 / 2) - 1 # middle row of pixels
 
 
-##
-# Draw a layer with the same format as the view for the next frame.
-# Blocks to next frame.
-# Consider passing a memoryview for efficiency.
-def display(layer):
-    for x in range(0, VIEW_W):
-        thumby.display.blit(memoryview(layer)[x*VIEW_H:(x+1)*VIEW_H], x, 0, 1, VIEW_H*8, -1, 0, 0)
-    thumby.display.update()
 
 
 ##
@@ -26,6 +18,14 @@ landfill = bytearray(VIEW_W*VIEW_H) # ground, platforms and roof layer (cleared 
 trap = bytearray(VIEW_W*VIEW_H) # npcs and traps that kill the players
 umby = bytearray(VIEW_W*VIEW_H) # the player: umby
 glow = bytearray(VIEW_W*VIEW_H) # the player: glow
+
+
+
+
+
+backOffsetX = 0
+caveOffsetX = 0
+landOffsetX = 0
 
 
 
@@ -43,7 +43,7 @@ def room_pattern(x, y):
 ##
 # Drawable pattern - basic dotted fences at roof and floor
 def fence_pattern(x, y):
-    return 1 if (abs(y-MID) > MID - 12) and not ((x % 3) or (y % 3)) else 0
+    return 1 if (abs(y-MID) > MID - 12) and not ((x % 4) or (y % 4)) else 0
 
 ##
 #
@@ -69,24 +69,38 @@ fill(wall_pattern, 0, 0, memoryview(back))
 fill(room_pattern, 0, 0, memoryview(land))
 fill(fence_pattern, 0, 0, memoryview(cave))
 
-cave[5] = 5
-cave[35] = 5
-cave[107] = 5
-cave[159] = 5
-cave[35*5+4] = 3
+land[5] = 5
+land[35] = 5
+land[107] = 5
+land[159] = 5
+land[35*5+4] = 3
+
+thumby.display.setFPS(60)
+
+
 
 
 t = 1;
 while(1):
+    
+    backOffsetX += 1 if t % 4 == 0 else 0
+    caveOffsetX += 1 if t % 2 == 0 else 0
+    landOffsetX += 1
+    
 
-    #fill(wall_pattern, t/4, 0, memoryview(back))
-    fill(room_pattern, t/2, 0, memoryview(land))
-    fill(fence_pattern, t, 0, memoryview(cave))
-
+    #fill(wall_pattern, int(t/4), 0, memoryview(back))
+    #fill(fence_pattern, int(t/2), 0, memoryview(cave))
+    #fill(room_pattern, t, 0, memoryview(land))
+    
     # Composite a view with new frame data, drawing to screen
-    display(memoryview(bytearray(
-        back[b] | cave[b] | land[b]
-        for b in range(0, VIEW_W*VIEW_H))))
+    thumby.display.blit(memoryview(bytearray(
+        back[(x+backOffsetX)%VIEW_W*VIEW_H+y]
+        | cave[(x+caveOffsetX)%VIEW_W*VIEW_H+y]
+        | land[(x+landOffsetX)%VIEW_W*VIEW_H+y]
+        for y in range(0, VIEW_H) for x in range(0, VIEW_W)
+        )), 0, 0, VIEW_W, VIEW_H*8, -1, 0, 0)
+    thumby.display.update()
+
 
     t += 1
 
